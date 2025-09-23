e13f0c32d7330594d4fdb66db09efcfde79baf93
Adam <brizakadam@gmail.com>
2025-09-23 17:28:13 +0200

    triangle but better

--- first HW.py
+++ first HW.py
@@ -21,10 +21,23 @@ print("Semi-perimeter/Polovičný obvod: ", s_per)
 ar = float(m.sqrt(s_per * (s_per - a) * (s_per - b) * (s_per - c)))
 print("Area/Obsah: ", ar)
 
+#medians
+ma = 1/2 * m.sqrt(2*(b**2) + 2*(c**2) - a**2)
+mb = 1/2 * m.sqrt(2*(a**2) + 2*(c**2) - b**2)
+mc = 1/2 * m.sqrt(2*(b**2) + 2*(a**2) + c**2)
+
+print(f"Medians/Tiažnice: a={ma}, b={mb}, c={mc}")
+
 #altitudes
-ha = 1/2 * m.sqrt(2*(b**2) + 2*(c**2) - a**2)
-hb = 1/2 * m.sqrt(2*(a**2) + 2*(c**2) - b**2)
-hc = 1/2 * m.sqrt(2*(b**2) + 2*(a**2) + c**2)
+ha = (2*ar)/a
+hb = (2*ar)/b
+hc = (2*ar)/c
 
-print(f"Altitudes/Výšky: a={ha}, b={hb}, c={hc}")
+print(f"Altitudes/Výšky: Va={ha}, Vb={hb}, Vc={hc}")
 
+#angles
+aa = m.degrees(m.acos(((b**2)+(c**2)-(a**2))/(2*b*c)))
+ab = m.degrees(m.acos(((a**2)+(c**2)-(b**2))/(2*a*c)))
+ac = m.degrees(m.acos(((a**2)+(b**2)-(c**2))/(2*b*a)))
+
+print(f"Angles/Uhly (Degrees/Stupne): α:{aa}, β:{ab}, γ:{ac}")
